75e1e2a57c5d55fd36be73a4f6d2ddfe7f74300b
Burty11 <142454463+Burty11@users.noreply.github.com>
2024-02-05 13:39:32 +0100

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,13 @@
+public class RandomDataGenerator {
+        for (int i = 0; i < 10; i++) {
+}
+
+        int[] data = new int[10];
+        }
+            data[i] = (int) (Math.random() * 100) + 1;
+    }
+
+        for (int item : data) {
+            System.out.println("Random Number: " + item);
+    public static void main(String[] args) {
+        }
